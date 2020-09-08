8373dd32701ac6f7ab2b243be07a42f1594de3ef
Takeshi Kaneko <target.ark@gmail.com>
2020-09-09 02:16:24 +0900

    Add a test to scc

--- src/acl/scc.d
+++ src/acl/scc.d
@@ -25,3 +25,41 @@ public:
     return internal.scc();
   }
 }
+
+unittest {
+  import std.algorithm : sort;
+  import std.array : array;
+
+  // https://atcoder.jp/contests/practice2/tasks/practice2_g
+  // Sample 1
+  long n = 6;
+  long m = 7;
+  long[2][] edges = [
+    [1, 4],
+    [5, 2],
+    [3, 0],
+    [5, 5],
+    [4, 1],
+    [0, 3],
+    [4, 2],
+  ];
+
+  long[][] expected = [
+    [1, 5],
+    [2, 4, 1],
+    [1, 2],
+    [2, 3, 0],
+  ];
+
+  auto graph = SccGraph(n);
+  foreach (e; edges) {
+    graph.addEdge(e[0], e[1]);
+  }
+  auto actual = graph.scc();
+
+  assert(actual.length == expected.length);
+  foreach (i; 0 .. actual.length) {
+    assert(actual[i].length == expected[i][0]);
+    assert(actual[i].dup.sort!"a<b".array == expected[i][1 .. $].dup.sort!"a<b".array);
+  }
+}
